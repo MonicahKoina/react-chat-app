39f10f0f358224eb283a038e4ec90a8a5b79ee09
Monicah Koina <monicahkoina1@gmail.com>
2025-03-28 12:20:18 +0300

    Welcome screen with a smooth animated gif

---
+++ src/pages/GetStarted.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import pic from "../assets/getstarted.gif";
+import { Button } from "antd";
+
+function GetStarted() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <figure className="flex items-center justify-center">
+        <img
+          src={pic}
+          alt="getstarted"
+          className="rounded-full w-80 h-80 object-cover mx-auto"
+        />
+      </figure>
+
+      <section className="flex flex-col justify-between gap-4 text-center mt-6">
+        <h3 className="text-2xl font-semibold text-gray-800">
+          Welcome to Qonvoo
+        </h3>
+        <p className="text-gray-600">
+          Unleash Seamless Conversations with Qonvoo!
+        </p>
+        <Button
+          type="primary"
+          className="bg-blue-500 text-white px-6 py-2 rounded-md"
+        >
+          Get Started
+        </Button>
+      </section>
+    </div>
+  );
+}
+
+export default GetStarted;
