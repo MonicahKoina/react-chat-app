ea0e6b1b11d61c8b25750acfb00d318061f9bf34
Monicah Koina <monicahkoina1@gmail.com>
2025-03-28 12:19:02 +0300

    added route for Get Started page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Splash from "../pages/Splash";
+import GetStarted from "../pages/GetStarted";
 
 function AppRoutes() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Splash />} />
+        <Route path="/getstarted" element={<GetStarted />}></Route>
       </Routes>
     </>
   );
